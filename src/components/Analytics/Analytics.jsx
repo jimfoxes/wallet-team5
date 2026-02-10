a9754f2b302170940cbdde2c8c6763c4c48fe913
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2026-02-10 15:40:11 +0300

    Сделана верстка календаря (без логики)

--- src/components/Analytics/Analytics.jsx
+++ src/components/Analytics/Analytics.jsx
@@ -1,7 +1,25 @@
 import * as S from './Analytics.styled'
+import Calendar from '../Calendar/Calendar'
 
 const Analytics = () => {
-    return <S.analyticsTitle>Страница анализа расходов</S.analyticsTitle>
+    return (
+        <S.AnalyticsContainer>
+            <S.AnalyticsTitle>Анализ расходов</S.AnalyticsTitle>
+
+            <S.ColumnsLayout>
+                <S.CalendarColumn>
+                    <Calendar
+                        selectedDate="16.01.2026"
+                        onDateSelect={(date) => console.log(date)}
+                    />
+                </S.CalendarColumn>
+
+                <S.ChartColumn>
+                    <h1>Диаграмма</h1>
+                </S.ChartColumn>
+            </S.ColumnsLayout>
+        </S.AnalyticsContainer>
+    )
 }
 
 export default Analytics
--- src/components/Analytics/Analytics.styled.js
+++ src/components/Analytics/Analytics.styled.js
@@ -1,23 +1,53 @@
 import styled from 'styled-components'
 
-export const analyticsTitle = styled.h1`
-    text-align: center;
-    margin: 0;
-    padding-top: 100px;
-    font-size: 4rem;
-    font-weight: 300;
-    letter-spacing: 2px;
-    color: #2c3e50;
-    position: relative;
+export const AnalyticsContainer = styled.div`
+    padding-right: calc(50% - 600px);
+    padding-left: calc(50% - 600px);
+    padding-top: 36px;
+    padding-bottom: 80px;
+    background-color: rgba(244, 245, 246, 1);
+`
 
-    &::after {
-        content: '';
-        position: absolute;
-        bottom: -15px;
-        left: 50%;
-        transform: translateX(-50%);
-        width: 100px;
-        height: 3px;
-        background: linear-gradient(90deg, #3498db, #2ecc71);
+export const AnalyticsTitle = styled.h1`
+    font-family: Montserrat;
+    font-weight: 700;
+    font-style: Bold;
+    font-size: 32px;
+    color: rgba(0, 0, 0, 1);
+    line-height: 150%;
+    letter-spacing: 0px;
+    text-align: left;
+`
+
+export const ColumnsLayout = styled.div`
+    display: flex;
+    column-gap: 32px;
+    margin-top: 32px;
+
+    @media screen and (max-width: 1024px) {
+        flex-direction: column;
+        align-items: center;
     }
+`
+
+export const CalendarColumn = styled.div`
+    display: flex;
+    column-gap: 32px;
+    width: 379px;
+    height: 540px;
+    overflow: hidden;
+    border-radius: 30px;
+    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.08);
+
+    @media screen and (max-width: 1024px) {
+        flex-direction: column;
+        align-items: center;
+    }
+`
+
+export const ChartColumn = styled.div`
+    background: rgba(255, 255, 255, 1);
+    border-radius: 30px;
+    box-shadow: 0px 20px 67px -12px rgba(0, 0, 0, 0.13);
+    padding: 32px 32px 44px 32px;
 `
